0602db18e5ef51dea0d69e202231f2e35e3b02e4
Finja-Malin Scheltens <finja.scheltens@outlook.de>
2024-03-05 15:35:32 +0100

    clickable badge to filter

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,15 +1,36 @@
-import { View, Text, StyleSheet, ViewStyle } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ViewStyle,
+} from "react-native";
 import { AppColors } from "../constants/AppColors";
+
 interface Props {
   text?: string;
   style?: ViewStyle;
+  clickable?: boolean; // New prop to control clickability
 }
 
-export default function PrimaryButton({ text, style }: Props) {
+export default function PrimaryButton({ text, clickable = false }: Props) {
+  const [clicked, setClicked] = useState(false);
+
+  const handlePress = () => {
+    if (clickable) {
+      setClicked(!clicked);
+    }
+  };
+
   return (
-    <View style={[styles.badge, style]}>
-      <Text style={styles.badgeText}>{text}</Text>
-    </View>
+    <TouchableOpacity onPress={handlePress} activeOpacity={clickable ? 0.8 : 1}>
+      <View style={[styles.badge, clicked && styles.clicked]}>
+        <Text style={[styles.badgeText, clicked && styles.badgeTextClicked]}>
+          {text}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
@@ -18,13 +39,23 @@ const styles = StyleSheet.create({
     backgroundColor: AppColors.GREY_200,
     alignItems: "center",
     alignSelf: "flex-start",
-    paddingVertical: 7,
+    paddingVertical: 8,
     paddingHorizontal: 12,
     borderRadius: 6,
   },
   badgeText: {
     fontFamily: "Inter-Medium",
     fontSize: 14,
-    color: AppColors.GREY_900,
+    color: AppColors.GREY_700,
+  },
+  clicked: {
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    paddingVertical: 7,
+    paddingHorizontal: 11,
+  },
+  badgeTextClicked: {
+    color: "black",
   },
 });
--- constants/AppColors.tsx
+++ constants/AppColors.tsx
@@ -2,6 +2,7 @@ export enum AppColors {
   PRIMARY = "#E44A4A",
   SECONDARY = "#1C4FD8",
   GREY_900 = "#121527",
+  GREY_700 = "#374151",
   GREY_500 = "#6B7280",
   GREY_300 = "#E0E0E0",
   GREY_200 = "#EDEFF1",
